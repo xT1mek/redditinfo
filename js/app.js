1d347db216855d3145c2cb13a7d997fef53eefbd
Alexander <kozichalexander2001@gmail.com>
2022-02-21 15:11:12 +0300

    implemented output of posts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-document.getElementById('myDate').valueAsDate = new Date();
-
 function myDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -24,29 +22,40 @@ function clearPosts() {
   document.getElementById("listpost").innerHTML = "";
 }
 
-function addMorePosts(){
-  let forma = document.getElementById('myDropdown');
-  forma.addEventListener('submit', getFormValue);
-  
-  const data = document.querySelector('[name = "data"]'),
-        posts = document.querySelector('[name = "numberposts"]'),
-        comms = document.querySelector('[name = "numbercomms"]');
+const blok = document.querySelector(".app__posts-list");
+const posttype = document.querySelector(".post-type");
 
-  return {
-    data: data.value,
-    posts: posts.value,
-    comms: comms.value
-  }
-}
+const renderPosts = () => {
+  fetch(`https://www.reddit.com/top.json`)
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(res) {
+      let currPost, markup = ``;
+      const postsArr = res.data.children;
 
-function addPosts(){
-  clearPosts();
-  const a = addMorePosts();
-  for (let i = 0; i < a.posts; i++){
-    let listel = document.createElement('li');
-    listel.className = "app__posts-list-el";
-    listpost.append(listel);
-  }   
-  console.log(addMorePosts());
-}
+      clearPosts();
 
+      for (let i = 0; i < numpost.value; i++) {
+        currPost = postsArr[i].data;
+        markup += `
+          <a class="app__posts-list-post" target="_blank" href="https://www.reddit.com/${currPost.permalink}">
+            <div class="app__posts-list-post-title"> ${currPost.title} </div>
+              ${currPost.selftext} 
+              </br></br>
+              <div class="app__posts-list-post-image">
+                <img src="${currPost.url}" alt="no image" width="500px" heigth="500px" onerror="this.style.visibility = 'hidden'">
+              </div>
+            </div>
+            <div class="app__posts-list-post-author"> Posted by ${currPost.author} </div>
+          </a>`;
+      }
+      
+      blok.insertAdjacentHTML('afterbegin', markup);
+    })
+    .catch(function(err) {
+      console.log(err); 
+    });
+    
+};
+ renderPosts('top')
