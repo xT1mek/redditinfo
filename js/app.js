86a501dd3179e973ffba1bab75110ec0981bf17b
Alexander <kozichalexander2001@gmail.com>
2022-02-23 10:09:23 +0300

    fixed variables names

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-function myDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function toggleForm() {
+  document.getElementById("my-dropdown").classList.toggle("show");
 }
 
 function inputRange() {
-  let rangepost = document.getElementById('rngpost');
-  let numpost = document.getElementById('numpost');
-  numpost.value = rangepost.value;
+  let rangePost = document.getElementById('rng-post');
+  let numPost = document.getElementById('num-post');
+  numPost.value = rangePost.value;
 }
 
 function getFormValue(event) {
@@ -13,49 +13,44 @@ function getFormValue(event) {
 }
 
 function clearPosts() {
-  document.getElementById("listpost").innerHTML = "";
+  document.getElementById("list-post").innerHTML = "";
 }
 
-const blok = document.querySelector(".app__posts-list");
-const dropdown = document.querySelector(".app__posts-list-post-type");
+const postsContainer = document.querySelector(".app__posts-list");
+const postTypeDropdown = document.querySelector(".app__post-type");
     
-function postTypeChoose(){
-  let index = dropdown.selectedIndex;
-  let value = dropdown.options[index].value;
+function postTypeChoose (){
+  let index = postTypeDropdown.selectedIndex;
+  let value = postTypeDropdown.options[index].value;
 
   renderPosts(value);
 }
 
 const renderPosts = (postType) => {
   fetch(`https://www.reddit.com/${postType}.json`)
-
+    .then(res => res.json())
     .then(function(res) {
-      return res.json();
-    })
-
-    .then(function(res) {
-      let currPost, markup = ``;
+      let currPost, markup = '';
       const postsArr = res.data.children;
-
+      let numPost = document.getElementById('num-post');
+      
       clearPosts();
 
-      for (let i = 0; i < numpost.value; i++) {
-        currPost = postsArr[i].data;
+      for (let postIndex = 0; postIndex < numPost.value; postIndex++) {
+        currPost = postsArr[postIndex].data;
         markup += `
-          <a class="app__posts-list-post" target="_blank" href="https://www.reddit.com/${currPost.permalink}">
-            <div class="app__posts-list-post-title"> ${currPost.title} </div>
-              ${currPost.selftext} 
-              </br></br>
-              <div class="app__posts-list-post-image">
-                <img src="${currPost.url}" alt="no image" width="500px" heigth="500px" onerror="this.style.visibility = 'hidden'">
-              </div>
+          <a class="post" target="_blank" href="https://www.reddit.com/${currPost.permalink}">
+            <div class="post__title"> ${currPost.title} </div>
+            ${currPost.selftext} 
+            <div class="post__image">
+              <img src="${currPost.url}" alt="no image" width="500px" heigth="500px" onerror="this.style.visibility = 'hidden'">
             </div>
-            <div class="app__posts-list-post-author"> Posted by ${currPost.author} </div>
+            </div>
+            <div class="post__author"> Posted by ${currPost.author} </div>
           </a>`;
       }
-      blok.insertAdjacentHTML('afterbegin', markup);
+      postsContainer.insertAdjacentHTML('afterbegin', markup);
     })
-
     .catch(function(err) {
       console.log(err); 
     });
